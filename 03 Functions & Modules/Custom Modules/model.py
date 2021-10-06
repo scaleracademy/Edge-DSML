479638a053f444f5ab79bc5af0cf6604bd77a371
Mohit Uniyal <mohituniyal2010@gmail.com>
2021-10-06 20:36:58 +0530

    add functions & modules

---
+++ 03 Functions & Modules/Custom Modules/model.py
@@ -0,0 +1,11 @@
+def greeter(name):
+    return "Good morning " + name
+
+def main():
+    print("Whatever code")
+    print("My name is:", __name__)
+
+
+if __name__ == '__main__':
+    print("This is confidential code.")
+    main()
